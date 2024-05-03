799bd944e321bf789f628150afb8cb2b3cb052fb
Stephen Isienyi <stephen.isienyi@gmail.com>
2024-05-03 09:42:38 -0700

    merge conf. res.

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const {
-    Immutable,
-    constants,
-    Tag
-} = require( './dist' );
+var c = require( './dist' ).constants;
+var Immutable = require( './dist' ).Immutable;
 
-module.exports.default = Immutable;
-module.exports.Immutable = Immutable;
-module.exports.Tag = Tag;
-for( var c in constants ) {
-    module.exports[ c ] = constants[ c ];
-}
+module.exports = {
+    CLEAR_TAG: c.CLEAR_TAG,
+    default: Immutable,
+    DELETE_TAG: c.DELETE_TAG,
+    GLOBAL_SELECTOR: c.GLOBAL_SELECTOR,
+    Immutable: Immutable,
+    MOVE_TAG: c.MOVE_TAG,
+    NULL_SELECTOR: c.NULL_SELECTOR,
+    PUSH_TAG: c.PUSH_TAG,
+    REPLACE_TAG: c.REPLACE_TAG,
+    SET_TAG: c.SET_TAG,
+    SPLICE_TAG: c.SPLICE_TAG,
+    Tag: require( './dist' ).Tag,
+};
